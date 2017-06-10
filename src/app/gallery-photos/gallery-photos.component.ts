fa59551fa68395880d473edfe47531ed31106ffc
María José Sánchez Doria <marijsanz@gmail.com>
2017-06-10 11:08:00 +0200

    Limpieza de codigo

--- src/app/gallery-photos/gallery-photos.component.ts
+++ src/app/gallery-photos/gallery-photos.component.ts
@@ -48,8 +48,7 @@ export class GalleryPhotosComponent implements OnInit {
             this.imagenes = uris.map(uri => new Imagen(uri));
         });
     });
-
-    //this.readFromFile();
+  
   }
 
   public seleccionar(imagen: Imagen): void {
@@ -91,69 +90,4 @@ export class GalleryPhotosComponent implements OnInit {
     });
 
   }
-
-/*
-  public readFromFile() {
-    // var pathToFile = cordova.file.dataDirectory + fileName;
-    var errorHandler = function (e) {  
-        var msg = '';
-        debugger;
-        switch (e.code) {
-            case FileError.QUOTA_EXCEEDED_ERR:
-                msg = 'Storage quota exceeded';
-                break;
-            case FileError.NOT_FOUND_ERR:
-                msg = 'File not found';
-                break;
-            case FileError.SECURITY_ERR:
-                msg = 'Security error';
-                break;
-            case FileError.INVALID_MODIFICATION_ERR:
-                msg = 'Invalid modification';
-                break;
-            case FileError.INVALID_STATE_ERR:
-                msg = 'Invalid state';
-                break;
-            default:
-                msg = 'Unknown error';
-                break;
-        };
-
-        console.log('Error: ' + msg);
-    }
-
-
-
-    window.plugins.contentproviderplugin.query({
-		contentUri: "content://media/external/images/media",
-		projection: ["_data"],
-		selection: null,
-		selectionArgs: null,
-		sortOrder: null
-	}, (datos:any[]) => {
-        this.ngZone.run(() => {
-            this.urisImagenes.length = 0;
-            datos.forEach(dato => {
-                this.urisImagenes.push(dato._data);
-            });
-        });
-	}, function (err) {
-		console.log("error query");
-	});
-
-/*
-    window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory + '/DCIM/Camera', (dirEntry) => {
-        var directoryReader = dirEntry.createReader();
-        directoryReader.readEntries(files => {
-            files.forEach(file => {
-                console.log(file);
-                this.urisImagenes.push(file.toURL());
-            });          
-        }, errorHandler.bind(null));
-    }, errorHandler.bind(null));
-*/
-        
-//  }
-
-
 }
